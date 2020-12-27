7980827082173d9266a7dd0a2f7207f44b0b733d
jefferson lennon <jeffersonlennon1@outlook.com>
2020-12-26 21:05:48 -0300

    fazendo teste de uma requisição post

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.101:3333/comments'
+  baseURL: 'http://192.168.1.101:3333/comments/'
 });
 
 export default api;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import {SafeAreaView, View, FlatList, ScrollView, Text, StyleSheet, StatusBar } from 'react-native';
+import {SafeAreaView,FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 
 
@@ -7,22 +7,36 @@ export default function  App(){
 const [ comments, setComments ] = useState([]);
 
 useEffect(()=>{
-  api.get('/all').then(response =>{
-    setComments(response.data.docs);
-    console.log(comments)
+  api.get('all').then(response =>{
+   setComments(response.data.docs);
+    console.log(response.data.docs.name)
+  }).catch(error =>{
+    console.log(error)
   })
-}, [])
+}, [comments]);
+async function handleAddComment(){
+  const response = await api.post('/', {
+    name: "Pessoa física",
+    email: "jefferson@gmai.com.br",
+    comment: "muito bom!"
+  })
+}
+
 
   return (
     <>
+
       <SafeAreaView style={styles.container}>
         <FlatList
         data={comments}
         keyExtractor={ comment => comment._id}
-        renderItem={({item})=>(
-          <Text style={styles.title}>{item.name}</Text> 
+        renderItem={({item: comment })=>(
+          <Text style={styles.title}>{comment.name}</Text> 
         )}
         />
+        <TouchableOpacity activeOpacity={0.5} style={styles.buttom} onPress={handleAddComment}>
+          <Text style={styles.buttomText}>Adicionar novo Comentário</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -42,5 +56,17 @@ const styles = StyleSheet.create({
     color: '#fafafa',
     textAlign: 'center',
     fontFamily: 'sans-serif'
+  },
+  buttom:{
+    backgroundColor: '#fafafa',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    margin: 20 
+  },
+  buttomText:{
+    color: '#7159c1',
+    textAlign: 'center'
   }
 })
